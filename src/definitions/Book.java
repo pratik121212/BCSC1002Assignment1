ae1483c4e132dcedb48330119f7f1e61d5c3044c
Pratik Sharma <pratik.sharma_cs19@gla.ac.in>
2020-08-26 00:18:07 +0530

    add private fields in class Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -1,10 +1,21 @@
 /*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:49 PM
+ *  User: Pratik Sharma (pratik121212)
+ *  Date: 26/08/20
+ *  Time: 12:00 AM
  *  File Name : Book.java
  * */
 package definitions;
 
 public class Book {
+    // Components of a Definition Class in Java
+    // 1. private fields
+    // 2. public getters and setters
+    // 3. Constructors for initializing the fields
+    // 4. toString() method
+    // 5. equals() and hashCode() method
+
+    private String bookName;
+    private String bookAuthor;
+    private String bookIsbnNumber;
 }
+
