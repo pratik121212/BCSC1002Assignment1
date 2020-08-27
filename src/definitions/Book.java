733943baaa2df25b27dda9c7d0f663433636a33d
Pratik Sharma <pratik.sharma_cs19@gla.ac.in>
2020-08-27 15:50:44 +0530

    add getters and setter methods in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -17,5 +17,30 @@ public class Book {
     private String bookName;
     private String bookAuthor;
     private String bookIsbnNumber;
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public String getBookIsbnNumber() {
+        return bookIsbnNumber;
+    }
+
+    public void setBookIsbnNumber(String bookIsbnNumber) {
+        this.bookIsbnNumber = bookIsbnNumber;
+    }
+
 }
 
